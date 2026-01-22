e02f4b5fe25eb7b9886aba848a4505bfe2ed54a5
Mononospace <38171201@users.noreply.github.com>
2026-01-22 15:20:50 +0900

    Add GuiElementDebugVertical/HorizontalLayout

---
+++ GUIElements/Layout/GuiElementDebugHorizontalLayout.cs
@@ -0,0 +1,22 @@
+﻿using Cairo;
+using System.Collections.Generic;
+using System.Linq;
+using Vintagestory.API.Client;
+using Vintagestory.API.MathTools;
+
+namespace MNGUITest.MNGUI.GUIElements.Layout;
+public class GuiElementDebugHorizontalLayout : GuiElement {
+    public GuiElementDebugHorizontalLayout(ICoreClientAPI capi, ElementBounds bounds) : base(capi, bounds) {
+    }
+
+    public override int OutlineColor() {
+        var intVal = ColorUtil.ToRgba(255, 255, 255, 0);
+        return intVal;
+    }
+
+    public override void ComposeElements(Context ctxStatic, ImageSurface surface) {
+        Bounds.CalcWorldBounds();
+    }
+
+
+}
